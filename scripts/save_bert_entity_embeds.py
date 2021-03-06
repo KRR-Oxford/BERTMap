d3e6da17f3ce8eb43e3da6b80c6a2c0780b7d4ea
Yuan He <yuahe@krr-linux-2.cs.ox.ac.uk>
2021-03-06 23:11:28 +0000

    add entity embed script

--- scripts/save_bert_entity_embeds.py
+++ scripts/save_bert_entity_embeds.py
@@ -7,17 +7,19 @@ base = "/home/yuahe/projects/OntoAlign-py/largebio_data/onto_labels"
 
 bert = PretrainedBert("emilyalsentzer/Bio_ClinicalBERT")
 bert_ent = BertEntityEmbedding(bert)
-bert_method_suffix = "last_2_mean"
-bert_method = "batch_sent_embeds_" + bert_method_suffix
 
-for src, tgt in [("fma", "nci"), ("fma", "snomed"), ("snomed", "nci")]:
-    
-    src_data = f"{base}/{src}2{tgt}.small.labels.tsv"
-    tgt_data = f"{base}/{tgt}2{src}.small.labels.tsv"
-    
-    
-    src_ent_embeds = bert_ent.entity_embeds_from_ontology(bert_method, src_data, batch_size=1000)
-    tgt_ent_embeds = bert_ent.entity_embeds_from_ontology(bert_method, tgt_data, batch_size=1000)
-    
-    torch.save(src_ent_embeds, f"{base}/{src}2{tgt}.small.embeds.pt")
-    torch.save(tgt_ent_embeds, f"{base}/{tgt}2{src}.small.embeds.pt")
+for suffix in ["last_2_mean", "last_1_cls"]:
+    bert_method = "batch_sent_embeds_" + suffix
+    name = "mean" if suffix == "last_2_mean" else None
+    name = "cls" if suffix == "last_1_cls" else None
+
+    for src, tgt in [("fma", "nci"), ("fma", "snomed"), ("snomed", "nci")]:
+        
+        src_data = f"{base}/{src}2{tgt}.small.labels.tsv"
+        tgt_data = f"{base}/{tgt}2{src}.small.labels.tsv"
+        
+        src_ent_embeds = bert_ent.entity_embeds_from_ontology(bert_method, src_data, batch_size=1000)
+        torch.save(src_ent_embeds, f"{base}/{src}2{tgt}.small.{name}.pt")
+        
+        tgt_ent_embeds = bert_ent.entity_embeds_from_ontology(bert_method, tgt_data, batch_size=1000)
+        torch.save(tgt_ent_embeds, f"{base}/{tgt}2{src}.small.{name}.pt")
