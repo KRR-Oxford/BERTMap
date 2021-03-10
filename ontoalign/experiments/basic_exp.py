3226efcc86a725910e1c2c6d45d4a637a7fbfdd5
Lawhy <lawhy0729@gmail.com>
2021-03-10 02:46:35 +0000

    add eval plot method

--- ontoalign/experiments/basic_exp.py
+++ ontoalign/experiments/basic_exp.py
@@ -3,6 +3,7 @@ from ontoalign.onto import Ontology
 from ontoalign.onto import OntoEvaluator
 from ontoalign.utils import log_print
 import time
+import seaborn as sns 
 
 
 class OntoAlignExperiment:
@@ -51,3 +52,30 @@ class OntoAlignExperiment:
         result_df.loc[task_name] = [evaluator.P, evaluator.R, evaluator.F1, evaluator.num_illegal]
         result_df = result_df.round({"Precision": 3, "Recall": 3, "F1": 3})
         return result_df
+
+    @staticmethod
+    def plot_eval(eval_csv, start_col=0):
+        # process eval data
+        eval_df = pd.read_csv(eval_csv, index_col=0).iloc[start_col:].reset_index()
+        eval_df["Mappings"] = list(eval_df["index"].apply(lambda x: x.split(":")[0]))
+        eval_df["Threshold"] = list(eval_df["index"].apply(lambda x: x.split(":")[1]))
+        eval_df.drop(columns=["index"], inplace=True)
+        eval_df = eval_df.melt(id_vars=["Mappings", "Threshold"], value_vars=["Precision", "Recall", "F1"], var_name="Metric", value_name="Value")
+        
+        # set styles
+        sns.set(style='darkgrid', rc={"font.weight": "bold", "font.size": 15, 
+                                      "font.family": "Times New Roman", "axes.labelweight": "bold", 
+                                      "axes.titleweight": "bold", "axes.titlepad": 10})
+        # create FacetGrid plots for all mappings
+        g = sns.FacetGrid(eval_df, col="Mappings", hue="Metric", height=5, aspect=1, margin_titles=True)
+        g.map(sns.lineplot, "Threshold", "Value", alpha=.7, marker="o")
+        name_mappings = list(eval_df["Mappings"].drop_duplicates())
+        for i in range(len(name_mappings)):
+            name = name_mappings[i]
+            part = eval_df[eval_df["Mappings"] == name]
+            part_f1 = part[part["Metric"] == "F1"]
+            ax = g.axes.flat[i]
+            ax.axvline(part_f1["Threshold"].loc[part_f1["Value"].idxmax()], ls='--',c='r', label="max(F1)")
+            ax.legend(loc="upper left")
+        g.fig.suptitle("Plots of Precision, Recall, Macro-F1 against Threshold for Combined, SRC2TGT and TGT2SRC Mappings", y=1.02, fontsize=15, weight="bold")
+        return g
