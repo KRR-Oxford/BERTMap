daecf24f5b4c2988bbfc5c23295afc414ef64fe2
Yuan He <yuahe@krr-linux-2.cs.ox.ac.uk>
2021-04-05 19:11:47 +0100

    onto corpora

--- bertmap/corpora/__init__.py
+++ bertmap/corpora/__init__.py
@@ -1,2 +1,3 @@
 from bertmap.corpora.basic_corpus import OntologyCorpus
 from bertmap.corpora.intra_onto import IntraOntoCorpus
+from bertmap.corpora.cross_onto import CrossOntoCorpus
--- bertmap/onto/__init__.py
+++ bertmap/onto/__init__.py
@@ -1,3 +1,2 @@
 from bertmap.onto.onto import Ontology
 from bertmap.onto.onto_eval import OntoEvaluator
-from bertmap.onto.aligned_onto import ToBeAlignedOntologies
