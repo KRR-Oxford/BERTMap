fd9d219c6a4a556d2cb2d1d1dc9f1c45d2c4a4df
Yuan He <yuahe@krr-linux-2.cs.ox.ac.uk>
2021-04-24 21:14:12 +0100

    add huggingface dataset; fix mapping search

--- bertmap/bert/huggingface_dataset.py
+++ bertmap/bert/huggingface_dataset.py
@@ -5,7 +5,7 @@ import pandas as pd
 def load_onto_tsv_dataset(data_path, tokenizer: AutoTokenizer):
     df = pd.read_csv(data_path, sep='\t')
     dataset = Dataset.from_pandas(df)
-    dataset = dataset.map(lambda example: {'labels': [example['Synonymous']]}, remove_columns=['Synonymous'])  # example becomes "inputs": [sent 1, sent 2], "labels": 0 or 1
-    encoded_dataset = dataset.map(lambda examples: tokenizer([str(example['Label1']), str(example['Label2'])]
-                                                             , padding=True, max_length=512, truncation=True), batched=True)
+    dataset = dataset.map(lambda example: {'label': example['Synonymous']}, remove_columns=['Synonymous']) 
+    encoded_dataset = dataset.map(lambda examples: tokenizer(str(examples['Label1']), str(examples['Label2']), 
+                                                             padding=True, max_length=512, truncation=True), batched=True)
     return encoded_dataset
