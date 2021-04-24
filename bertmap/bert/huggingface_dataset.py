06deda6c209ed1b30188113313bfd0dbee73e1c9
Yuan He <yuahe@krr-linux-2.cs.ox.ac.uk>
2021-04-25 00:32:05 +0100

    fix dataset

--- bertmap/bert/huggingface_dataset.py
+++ bertmap/bert/huggingface_dataset.py
@@ -5,10 +5,10 @@ import pandas as pd
 def load_onto_tsv_dataset(data_path, tokenizer: AutoTokenizer):
     na_vals = pd.io.parsers.STR_NA_VALUES.difference({'NULL', 'null', 'n/a'})
     df = pd.read_csv(data_path, sep='\t', na_values=na_vals, keep_default_na=False)
+    df = df.rename(columns={'Synonymous': 'labels'})
     dataset = Dataset.from_pandas(df)
-    encoded_dataset = dataset.map(lambda examples: tokenizer(examples['Label1'], examples['Label2'], 
-                                                             padding=True, truncation=True), batched=True)
-    encoded_dataset = encoded_dataset.map(lambda example: {'labels': example['Synonymous']}, 
-                                          remove_columns=['Synonymous']) 
+    encoded_dataset = dataset.map(lambda examples: 
+        tokenizer(examples['Label1'], examples['Label2'], padding=True, truncation=True, max_length=512), 
+        batched=True, batch_size=1024)
     encoded_dataset.set_format("torch", columns=["input_ids", "attention_mask", "token_type_ids", "labels"])
     return encoded_dataset
