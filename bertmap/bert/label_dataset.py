790996c89acfec77b57173528d67ff516854f769
Yuan He <yuahe@krr-linux-2.cs.ox.ac.uk>
2021-04-24 03:10:22 +0100

    fix dataset

--- bertmap/bert/label_dataset.py
+++ bertmap/bert/label_dataset.py
@@ -13,10 +13,12 @@ class OntoLabelDataset(Dataset):
         for _, dp in data.iterrows():
             text_pairs.append([str(dp["Label1"]), str(dp["Label2"])])
             self.labels.append(dp["Synonymous"])
-        self.encode = lambda x: tokenizer(x, padding=True, max_length=512, truncation=True)
-        self.set_transform(self.encode)
+        self.encodings = tokenizer(text_pairs, padding=True, max_length=512, truncation=True)  # truncation is no need as there is no long sentence here
 
     def __len__(self):
         return len(self.labels)
-
     
+    def __getitem__(self, idx):
+        item = {k: torch.tensor(v[idx]) for k, v in self.encodings.items()}
+        item['labels'] = torch.tensor(self.labels[idx])
+        return item
