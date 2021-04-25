ecbf4e42c01c72aabc78e9a1a2617f9d9277b23c
Yuan He <yuahe@krr-linux-2.cs.ox.ac.uk>
2021-04-25 04:55:08 +0100

    fix dataset

--- bertmap/bert/label_dataset.py
+++ bertmap/bert/label_dataset.py
@@ -5,12 +5,13 @@ import torch
 
 class OntoLabelDataset(Dataset):
 
-    def __init__(self, data_tsv, tokenizer: AutoTokenizer):
+    def __init__(self, data_tsv, tokenizer: AutoTokenizer, max_length=512):
         # Model parameter
         na_vals = pd.io.parsers.STR_NA_VALUES.difference({'NULL', 'null', 'n/a'})
         data = pd.read_csv(data_tsv, sep='\t', na_values=na_vals, keep_default_na=False)
         self.labels = data["Synonymous"]
-        self.encodings = tokenizer(list(zip(data["Label1"], data["Label2"])), padding=True)  
+        self.encodings = tokenizer(list(zip(data["Label1"], data["Label2"])), 
+                                   padding=True, max_length=max_length, truncation=True)  
         # truncation is no need as there is no long sentence here
 
     def __len__(self):
