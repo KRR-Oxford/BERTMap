63cb7e412f0336948013d91e82b11548577b06be
Yuan He <yuahe@krr-linux-2.cs.ox.ac.uk>
2021-04-25 01:46:01 +0100

    fix dataset

--- bertmap/bert/huggingface_dataset.py
+++ bertmap/bert/huggingface_dataset.py
@@ -1,5 +1,6 @@
 from transformers import AutoTokenizer
 from datasets import Dataset
+import torch
 import pandas as pd
 
 def load_onto_tsv_dataset(data_path, tokenizer: AutoTokenizer, batch_size=1024, max_length=512):
@@ -7,8 +8,9 @@ def load_onto_tsv_dataset(data_path, tokenizer: AutoTokenizer, batch_size=1024, 
     df = pd.read_csv(data_path, sep='\t', na_values=na_vals, keep_default_na=False)
     dataset = Dataset.from_pandas(df)
     def encode(examples):
-        item = tokenizer(examples['Label1'], examples['Label2'], return_tensor='pt', 
+        item = tokenizer(examples['Label1'], examples['Label2'], return_tensors='pt', 
                          max_length=max_length, padding='longest', truncation=True)
-        item['labels'] = examples['Synonymous']
+        item['labels'] = torch.tensor(examples['Synonymous'])
+        return item
     dataset.set_transform(encode)
     return dataset
