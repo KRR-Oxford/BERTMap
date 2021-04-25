5e77cd780ae2248dc8667f6f6bf15ccd7ff66447
Yuan He <yuahe@krr-linux-2.cs.ox.ac.uk>
2021-04-25 03:43:02 +0100

    fix trainer

--- bertmap/bert/label_dataset.py
+++ bertmap/bert/label_dataset.py
@@ -7,20 +7,17 @@ class OntoLabelDataset(torch.utils.data.Dataset):
 
     def __init__(self, data_tsv, tokenizer: AutoTokenizer, max_length=512):
         # Model parameter
-        data = pd.read_csv(data_tsv, sep="\t")
-        text_pairs = []
-        self.labels = []
-        for _, dp in data.iterrows():
-            text_pairs.append([str(dp["Label1"]), str(dp["Label2"])])
-            self.labels.append(dp["Synonymous"])
-        self.encode = lambda x: tokenizer(x, return_tensors='pt', max_length=max_length, padding="longest", truncation=True)
+        self.data = pd.read_csv(data_tsv, sep="\t")
+        self.labels = list(self.data["Synonymous"])
+        self.encode = lambda examples: tokenizer(examples["Label1"], examples["Label2"], return_tensors='pt', 
+                                                 max_length=max_length, padding="longest", truncation=True)
         # self.encodings = tokenizer(text_pairs, truncation=True, padding=True)  # truncation is no need as there is no long sentence here
 
     def __len__(self):
         return len(self.labels)
     
     def __getitem__(self, idx):
-        item = self.encode(self.text_pairs[idx])
+        item = self.encode(self.data.iloc[idx])
         # item = {k: torch.tensor(v[idx]) for k, v in self.encodings.items()}
         item['labels'] = torch.tensor(self.labels[idx])
         return item
