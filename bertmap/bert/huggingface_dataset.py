dcfdd3005008760f0c5aeaa4686f11ad40774baf
Yuan He <yuahe@krr-linux-2.cs.ox.ac.uk>
2021-04-25 03:44:54 +0100

    fix trainer

--- bertmap/bert/huggingface_dataset.py
+++ bertmap/bert/huggingface_dataset.py
@@ -5,13 +5,13 @@ from datasets import Dataset
 import torch
 import pandas as pd
 
-def load_onto_tsv_dataset(data_path, tokenizer: AutoTokenizer, max_length=512):
+def load_onto_tsv_dataset(data_tsv, tokenizer: AutoTokenizer, max_length=512):
     na_vals = pd.io.parsers.STR_NA_VALUES.difference({'NULL', 'null', 'n/a'})
-    df = pd.read_csv(data_path, sep='\t', na_values=na_vals, keep_default_na=False).\
-        rename(columns={"Label1": "sentence1", "Label2": "sentence2", "Synonymous": "labels"})
+    df = pd.read_csv(data_tsv, sep='\t', na_values=na_vals, keep_default_na=False).\
+        rename(columns={"Synonymous": "labels"})
     dataset = Dataset.from_pandas(df)
     def encode(examples):
-        items = tokenizer(examples['sentence1'], examples['sentence1'], return_tensors='pt', 
+        items = tokenizer(examples['Label1'], examples['Label2'], return_tensors='pt', 
                         max_length=max_length, padding='max_length', truncation=True)
         items['labels'] = torch.tensor(examples['labels'])
         return items
--- bertmap/bert/label_dataset.py
+++ bertmap/bert/label_dataset.py
@@ -7,7 +7,8 @@ class OntoLabelDataset(torch.utils.data.Dataset):
 
     def __init__(self, data_tsv, tokenizer: AutoTokenizer, max_length=512):
         # Model parameter
-        self.data = pd.read_csv(data_tsv, sep="\t")
+        na_vals = pd.io.parsers.STR_NA_VALUES.difference({'NULL', 'null', 'n/a'})
+        self.data = pd.read_csv(data_tsv, sep='\t', na_values=na_vals, keep_default_na=False)
         self.labels = list(self.data["Synonymous"])
         self.encode = lambda examples: tokenizer(examples["Label1"], examples["Label2"], return_tensors='pt', 
                                                  max_length=max_length, padding="longest", truncation=True)
