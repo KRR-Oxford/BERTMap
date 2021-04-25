b480f89329875d5811635909e32d764be80744ba
Yuan He <yuahe@krr-linux-2.cs.ox.ac.uk>
2021-04-25 01:50:06 +0100

    fix dataset

--- bertmap/bert/huggingface_dataset.py
+++ bertmap/bert/huggingface_dataset.py
@@ -7,6 +7,7 @@ def load_onto_tsv_dataset(data_path, tokenizer: AutoTokenizer, batch_size=1024, 
     na_vals = pd.io.parsers.STR_NA_VALUES.difference({'NULL', 'null', 'n/a'})
     df = pd.read_csv(data_path, sep='\t', na_values=na_vals, keep_default_na=False)
     dataset = Dataset.from_pandas(df)
+    print(df.head(10))
     def encode(examples):
         item = tokenizer(examples['Label1'], examples['Label2'], return_tensors='pt', 
                          max_length=max_length, padding='longest', truncation=True)
