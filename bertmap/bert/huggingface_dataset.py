7be6d792243e61181a92a1cf7238032a99f4ad4e
Yuan He <yuahe@krr-linux-2.cs.ox.ac.uk>
2021-04-25 01:33:41 +0100

    fix dataset

--- bertmap/bert/huggingface_dataset.py
+++ bertmap/bert/huggingface_dataset.py
@@ -7,8 +7,6 @@ def load_onto_tsv_dataset(data_path, tokenizer: AutoTokenizer, batch_size=1024, 
     df = pd.read_csv(data_path, sep='\t', na_values=na_vals, keep_default_na=False)
     df = df.rename(columns={'Synonymous': 'labels'})
     dataset = Dataset.from_pandas(df)
-    encoded_dataset = dataset.map(lambda examples: 
-        tokenizer(examples['Label1'], examples['Label2'], max_length=max_length, padding='max_length', truncation=True), 
-        batched=True, batch_size=1024)
-    encoded_dataset.set_format("torch", columns=["input_ids", "attention_mask", "token_type_ids", "labels"])
-    return encoded_dataset
+    dataset.set_transform(lambda examples: 
+        tokenizer(examples['Label1'], examples['Label2'], max_length=max_length, padding='longest', truncation=True))
+    return dataset
