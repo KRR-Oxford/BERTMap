3113de97efd0b407ff1f3c3dd03bf1953fff99ed
Yuan He <yuahe@krr-linux-2.cs.ox.ac.uk>
2021-05-02 22:34:19 +0100

    rewrite corpora done

--- bertmap/corpora/basic_corpus.py
+++ bertmap/corpora/basic_corpus.py
@@ -1,91 +1,126 @@
 """
-Ontology Corpus superclass, it requires implementation of how to create sub-corpora.
+Ontology Corpus superclass, it requires implementation of how to create corpora from the classtexts.
+
+The corpus essentially stores pairs of texts that are semantically related, including synonyms and nonsynonyms:
+
+    Synonyms are divided into *forward*, *backward* and *identity* synonyms;
+    Nonsynonyms are divided into *soft* (forward/backward) and *hard* (forward/backward) nonsynonyms.
+    
+    {Identity synonyms} are pairs of duplicated classtexts -> to learn Reflexivity or String matching.
+
+    {Forward synonyms} are pairs of *distinct* classtexts belong to one class, while {backward synonyms} are just the reversed
+        -> to learn Symmertry and Transitiviy.
+        
+    {Soft nonsynonyms} are pairs of classtexts from random choices of different classes. 
+    
+    {Hard nonsynonyms} are pairs of classtexts from *disjoint* classes (e.g. sibling classes up to certain depth)
+    
+    The concepts of forward and backward apply to nonsynonyms as well.
 """
 
 import json
 import pandas as pd
+from typing import Optional
 
 class OntologyCorpus:
     
-    def __init__(self, *config_args, onto_name=None, corpus_path=None):
-        self.onto_name = onto_name
-        if not corpus_path:
-            self.init_config(*config_args)
-            self.create_corpus()
-        else:
-            # load corpus from local storage
-            self.load_corpus(save_dir=corpus_path)
-        assert self.corpus_dict is not None
+    def __init__(self, *corpus_args, corpus_file: Optional[str]=None):
+        if corpus_file: self.load_corpus(corpus_file)
+        else: self.config(*corpus_args); self.create_corpus()
+        assert self.corpus_type is not None
+        assert self.corpus_info is not None
+        assert self.corpus is not None
         
-    def init_config(self, *config_args):
+    def config(self, *config_args):
         raise NotImplementedError
         
     def create_corpus(self):
         raise NotImplementedError
+    
+    def __repr__(self):
+        corpus_info = self.corpus[" corpus_info "]
+        onto = corpus_info["onto"]
+        report = f"<OntologyCorpus type={self.corpus_type} onto={onto}>\n"
+        synonym_report = "\t<Synonyms "
+        for k, v in corpus_info["synonyms"].items():
+            synonym_report += f"{k}={v} "
+        synonym_report += ">\n"
+        nonsynonym_report = "\t<Nonsynonyms "
+        for k, v in corpus_info["nonsynonyms"].items():
+            nonsynonym_report += f"{k}={v} "
+        nonsynonym_report += ">\n"
+        report += synonym_report
+        report += nonsynonym_report
+        report += "</OntologyCorpus>"
+        return report
 
     @staticmethod
-    def term_dict():
+    def semantic_dict():
         # some corpus might not define the hard nonsynonyms so that they will be empty
         return {"synonyms": [], "soft_nonsynonyms": [], "hard_nonsynonyms": []}
-
-    def id_synonyms(self):
-        labels = list(self.corpus_dict.keys())[1:]  
-        return list(zip(labels, labels, [1]*len(labels)))
     
-    def extract_label_pairs(self):
+    def corpus_info(self):
+        """init corpus information storage
+        """
+        return {
+            "type": self.corpus_type,
+            "onto": [],
+            "synonyms": {"for+back": 0, "id": None},
+            "nonsynonyms": {"soft": 0, "hard": None, "removed_violations": 0}
+        }
+
+    def identity_synonyms(self):
+        """[Reflexivity]: Map each classtext to itself"""
+        classtexts = list(self.corpus.keys())[1:]  
+        return list(zip(classtexts, classtexts, [1]*len(classtexts)))
+    
+    def extract_semantic_pairs(self):
+        """Extract classtext pairs from corpus"""
         synonyms = []
         soft_nonsynonyms = []
         hard_nonsynonyms = []
-        for term, semantic_dict in self.corpus_dict.items():
+        for term, semantic_dict in self.corpus.items():
             if term == " corpus_info ":
                 continue
             synonyms += [(term, s, 1) for s in semantic_dict["synonyms"]]
             soft_nonsynonyms += [(term, ns, 0) for ns in semantic_dict["soft_nonsynonyms"]]
             hard_nonsynonyms += [(term, ns, 0) for ns in semantic_dict["hard_nonsynonyms"]]  
             
-        return {"id_synonyms": self.id_synonyms(), "synonyms": synonyms,
+        return {"id_synonyms": self.identity_synonyms(), "synonyms": synonyms,
                 "soft_nonsynonyms": soft_nonsynonyms, "hard_nonsynonyms": hard_nonsynonyms}
     
     @staticmethod
-    def backward_label_pairs(label_pairs):
+    def backward_semantic_pairs(semantic_pairs):
         # (label1, label2, whether_or_not_synonymous)
-        return [(y, x, s) for x, y, s in label_pairs]
+        return [(y, x, s) for x, y, s in semantic_pairs]
     
     @staticmethod
-    def save_labels(label_data, save_dir_tsv):
-        pd.DataFrame(label_data, columns=["Label1", "Label2", "Synonymous"]).to_csv(save_dir_tsv, sep='\t', index=False)
+    def save_semantic_pairs(semantic_data, save_file):
+        """Save all the semantic sentence pairs"""
+        pd.DataFrame(semantic_data, columns=["sentence1", "sentence2", "label"]).to_csv(save_file, sep='\t', index=False)
     
-    def negative_sample_check(self, label1, label2):
+    def negative_sample_check(self, sent1, sent2):
         """The negative label pair (l1, l2) must satisfy the following conditions:
            1.  l1 not = l2; 
            2. (l1, l2) or (l2, l1) not a synonym; 
         """
         # edge case to prevent when label 1 or 2 has not been added into the dict 
-        if len(self.corpus_dict[label1]) == 0 or len(self.corpus_dict[label2]) == 0:
-            return True
+        if len(self.corpus[sent1]) == 0: return True
+        if len(self.corpus[sent2]) == 0: return True
         # conduct the negative sample check
-        equiv_cond = label1 == label2
-        partition_cond = label2 in self.corpus_dict[label1]["synonyms"] or \
-            label1 in self.corpus_dict[label2]["synonyms"]
+        equiv_cond = sent1 == sent2
+        partition_cond = sent2 in self.corpus[sent1]["synonyms"] or \
+            sent1 in self.corpus[sent2]["synonyms"]
         if equiv_cond or partition_cond:
             return False
         return True
 
-    def save_corpus(self, save_dir):
-        save_file = save_dir + f"/{self.onto_name}.{self.corpus_type}.json"
-        with open(save_file, "w") as f:
-            json.dump(self.corpus_dict, f, indent=4, separators=(',', ': '), sort_keys=True)
+    def save_corpus(self, corpus_file):
+        with open(corpus_file, "w") as f:
+            json.dump(self.corpus, f, indent=4, separators=(',', ': '), sort_keys=True)
             
-    def load_corpus(self, save_dir):
-        save_file = save_dir + f"/{self.onto_name}.{self.corpus_type}.json"
-        with open(save_file, "r") as f:
-            self.corpus_dict = json.load(f)
-        self.report(self.corpus_dict)
-        
-    @staticmethod
-    def report(corpus_dict):
-        print("--------------- Corpus Summary --------------")
-        corpus_info = corpus_dict[" corpus_info "]
-        for k, v in corpus_info.items():
-            print(f"{k}: {v}")
-        print("---------------------------------------------")
+    def load_corpus(self, corpus_file):
+        with open(corpus_file, "r") as f:
+            self.corpus = json.load(f)
+        self.corpus_info = self.corpus[" corpus_info "]
+        self.corpus_type = self.corpus_info["type"]
