91f3d34ee006a7b886482c5dcf733160138dca85
Yuan He <yuahe@krr-linux-2.cs.ox.ac.uk>
2021-05-10 15:42:34 +0100

    formatting

--- bertmap/utils/general_utils.py
+++ bertmap/utils/general_utils.py
@@ -1,77 +1,91 @@
 import sys
 import random
 
+
 def banner(info=None, banner_len=60, sym="-"):
     print()
-    if not info: 
+    if not info:
         print(sym * banner_len)
-    else: 
+    else:
         info = sym * ((banner_len - len(info)) // 2 - 1) + " " + info
         info = info + " " + sym * (banner_len - len(info) - 1)
         print(info)
     print()
 
+
 def evenly_divide(start, end, num_splits):
     step = (end - start) / num_splits
     return [start + step * i for i in range(num_splits + 1)]
 
+
 def random_drop(lst):
     lst.remove(lst[random.randrange(0, len(lst))])
     return lst
-    
+
+
 def exclude_randrange(start, end, exclude):
     result = random.randrange(start, end)
     while result == exclude and end - start > 1:
         result = random.randrange(start, end)
     return result
 
+
 def uniqify(ls):
     non_empty_ls = list(filter(lambda x: x != "", ls))
     return list(dict.fromkeys(non_empty_ls))
+
 
 def size_split(sizes):
     max_range = list(range(sum(sizes)))
     splits = []
     for i in range(len(sizes)):
-        start = sizes[i - 1] if i >=1 else 0
+        start = sizes[i - 1] if i >= 1 else 0
         end = sizes[i] + start
-        splits.append(max_range[start: end])
+        splits.append(max_range[start:end])
     return splits
 
+
 def batch_split(batch_size, max_num):
-    """Split into equal parts of {batch_size} as well as the tail"""
-    
+    """Split into equal parts of {batch_size} as well as the tail
+    """
+
     if batch_size > max_num:
         print("Fix the batch size to maximum number.")
         batch_size = max_num
-        
+
     max_range = list(range(max_num))
     num_splits = max_num // batch_size
-    num_splits = num_splits - 1 if max_num % batch_size == 0 else num_splits  # for edge case, there will be an empty batch
+    num_splits = (
+        num_splits - 1 if max_num % batch_size == 0 else num_splits
+    )  # for edge case, there will be an empty batch
     splits = []
     for i in range(num_splits + 1):
         start = i * batch_size
         end = min((i + 1) * batch_size, max_num)  # dealing the tail part
-        splits.append(max_range[start: end])
+        splits.append(max_range[start:end])
     assert len(splits) == num_splits + 1
     return splits
-    
+
+
 def equal_split(num_splits, max_num):
-    """Split into equal {num_splits} part as well as the tail"""
+    """Split into equal {num_splits} part as well as the tail
+    """
     max_range = list(range(max_num))
     interval_range = max_num // num_splits
     splits = []
     for i in range(num_splits + 1):
         start = i * interval_range
         end = min((i + 1) * interval_range, max_num)  # dealing the tail part
-        splits.append(max_range[start: end])
+        splits.append(max_range[start:end])
     assert len(splits) == num_splits + 1
     return splits
 
+
 def log_print(log_info, log_path: str):
-    """Logging information"""
+    """Logging information
+    """
     print(log_info)
-    with open(log_path, 'a+') as f:
-        f.write(f'{log_info}\n')
+    with open(log_path, "a+") as f:
+        f.write(f"{log_info}\n")
     # flush() is important for printing logs during multiprocessing
-    sys.stdout.flush()  
+    sys.stdout.flush()
