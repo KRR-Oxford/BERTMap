c999fe123bbdc3f8a972c961977ce5c39dc6181c
Yuan He <yuahe@krr-linux-2.cs.ox.ac.uk>
2021-05-10 15:43:30 +0100

    formatting

--- bertmap/utils/torch_utils.py
+++ bertmap/utils/torch_utils.py
@@ -2,18 +2,20 @@ import torch
 import random
 import numpy as np
 
+
 def get_device(device_num=0):
     # If there's a GPU available...
-    if torch.cuda.is_available():    
-        # Tell PyTorch to use the GPU.    
+    if torch.cuda.is_available():
+        # Tell PyTorch to use the GPU.
         device = torch.device(f"cuda:{device_num}")
-        print('There are %d GPU(s) available.' % torch.cuda.device_count())
-        print('We will use the GPU:', torch.cuda.get_device_name(device_num))
+        print("There are %d GPU(s) available." % torch.cuda.device_count())
+        print("We will use the GPU:", torch.cuda.get_device_name(device_num))
     # If not...
     else:
-        print('No GPU available, using the CPU instead.')
+        print("No GPU available, using the CPU instead.")
         device = torch.device("cpu")
     return device
+
 
 def set_seed(seed_val=888):
     random.seed(seed_val)
