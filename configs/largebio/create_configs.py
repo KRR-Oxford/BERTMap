aab1cbee5a05b03b5ef34d19800090adb5105f71
Yuan He <yuahe@krr-linux-2.cs.ox.ac.uk>
2021-05-14 00:47:12 +0100

    add configs

---
+++ configs/largebio/create_configs.py
@@ -0,0 +1,62 @@
+import json
+from pathlib import Path
+
+
+server_dir = "/home/cjy/BERTMap"
+cur_dir = "/home/yuahe/projects/BERTMap"
+
+with open(f"{cur_dir}/config.json", "r") as f:
+    config = json.load(f)
+
+for src, tgt in [("fma", "nci"), ("fma", "snomed"), ("snomed", "nci")]:
+    for learn in ["us", "ss"]:
+        for ids in [True, False]:
+            exp_dir = f"{cur_dir}/configs/largebio/{src}2{tgt}.small"
+            Path(exp_dir).mkdir(parents=True, exist_ok=True)
+            config["data"]["task_dir"] = server_dir + f"/{src}2{tgt}.small"
+            config["data"]["src_onto"] = src
+            config["data"]["tgt_onto"] = tgt
+            config["data"]["src_onto_file"] = (
+                server_dir + f"/data/largebio/ontos/{src}2{tgt}.small.owl"
+            )
+            config["data"]["tgt_onto_file"] = (
+                server_dir + f"/data/largebio/ontos/{tgt}2{src}.small.owl"
+            )
+            config["corpora"]["src2tgt_mappings_file"] = (
+                server_dir + f"/data/largebio/refs/{src}2{tgt}.tsv"
+            )
+            config["corpora"]["ignored_mappings_file"] = (
+                server_dir + f"/data/largebio/refs/{src}2{tgt}.ignored.tsv"
+            )
+            config["fine-tune"]["include_ids"] = ids
+            config["fine-tune"]["learning"] = learn
+            config_file = (
+                exp_dir + f"/{learn}.ids.config.json" if ids else exp_dir + f"/{learn}.config.json"
+            )
+            with open(config_file, "w") as c:
+                json.dump(config, c, indent=4, separators=(",", ": "))
+
+
+for src, tgt in [("fma", "nci"), ("fma", "snomed"), ("snomed", "nci")]:
+    for learn in ["us", "ss"]:
+        for ids in [True, False]:
+            exp_dir = f"{cur_dir}/configs/largebio/{src}2{tgt}.whole"
+            Path(exp_dir).mkdir(parents=True, exist_ok=True)
+            config["data"]["task_dir"] = server_dir + f"/{src}2{tgt}.whole"
+            config["data"]["src_onto"] = src
+            config["data"]["tgt_onto"] = tgt
+            config["data"]["src_onto_file"] = server_dir + f"/data/largebio/ontos/{src}.whole.owl"
+            config["data"]["tgt_onto_file"] = server_dir + f"/data/largebio/ontos/{tgt}.whole.owl"
+            config["corpora"]["src2tgt_mappings_file"] = (
+                server_dir + f"/data/largebio/refs/{src}2{tgt}.tsv"
+            )
+            config["corpora"]["ignored_mappings_file"] = (
+                server_dir + f"/data/largebio/refs/{src}2{tgt}.ignored.tsv"
+            )
+            config["fine-tune"]["include_ids"] = ids
+            config["fine-tune"]["learning"] = learn
+            config_file = (
+                exp_dir + f"/{learn}.ids.config.json" if ids else exp_dir + f"/{learn}.config.json"
+            )
+            with open(config_file, "w") as c:
+                json.dump(config, c, indent=4, separators=(",", ": "))
