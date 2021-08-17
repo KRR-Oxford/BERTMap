8708f1ab1e536d46191d74362176402c67821375
lawhy <lawhy0729@gmail.com>
2021-08-17 07:10:49 +0100

    extension modules added

--- bertmap/extend/onto_extend.py
+++ bertmap/extend/onto_extend.py
@@ -2,41 +2,55 @@
 Mapping Extension class
 """
 
-from typing import Union, List, Tuple
+from typing import List, Tuple, Dict, Union
 
-from bertmap.onto import OntoBox, OntoEvaluator
-from pandas import DataFrame
+import bertmap
+from bertmap.onto import OntoBox
 from owlready2.entity import ThingClass
+from pandas.core.frame import DataFrame
+import pandas as pd
 from itertools import product
+from copy import deepcopy
 
 
 class OntoExtend:
+
+    na_vals = bertmap.na_vals
+
     def __init__(
         self,
         src_ob: OntoBox,
         tgt_ob: OntoBox,
+        mapping_file: str,
         extend_threshold: float,
     ):
 
         self.src_ob = src_ob
         self.tgt_ob = tgt_ob
         self.threshold = extend_threshold
+        self.raw_mappings = self.read_mappings_to_dict(mapping_file, extend_threshold)
+        self.frontier = deepcopy(self.raw_mappings)  # the frontier of mapping expansion
+        self.expansion = dict()
 
-    def extend_mappings(self, pred_mappings: List[str]):
-        expansion = []
-        count = 0
-        for p_map in pred_mappings:
-            src_iri, tgt_iri = p_map.split("\t")
-            sup_maps, sub_maps = self.one_hob_extend(src_iri, tgt_iri)
-            print(f"[Map {count}]: {src_iri} -> {tgt_iri}")
-            print(f"\t# extended mappings in: parents={len(sup_maps)}; children={len(sub_maps)}")
-            expansion += sup_maps
-            expansion += sub_maps
-            count += 1
-        print(f"Extend {len(expansion)} mappings.")
-        return expansion
+    def extend_mappings(self, max_iter: int = 1):
+        num_iter = 0
+        while self.frontier and num_iter < max_iter:
+            count = 0
+            new_expansion = dict()
+            for mapping in self.frontier.keys():
+                src_iri, tgt_iri = mapping.split("\t")
+                print(f"[Map {count}]: {src_iri} -> {tgt_iri}")
+                sup_maps, sub_maps = self.one_hob_extend(src_iri, tgt_iri)
+                # merging dictionary is possible because the duplicates have been removed
+                new_expansion = {**new_expansion, **sup_maps, **sub_maps}
+                print(f"\t[Iteration {num_iter}]: Extend {len(new_expansion)} new mappings")
+                count += 1
+            num_iter += 1
+            self.frontier = deepcopy(new_expansion)
+            self.expansion = {**self.expansion, **new_expansion}
+            print(f"[Expansion]: total={len(self.expansion)}")
 
-    def one_hob_extend(self, src_iri: str, tgt_iri: str) -> Tuple[List, List]:
+    def one_hob_extend(self, src_iri: str, tgt_iri: str) -> Tuple[Dict, Dict]:
         """1-hop mapping extension, the assumption is given a highly confident mapping,
         the corresponding classes' parents and children are likely to be matched.
 
@@ -50,28 +64,42 @@ class OntoExtend:
         # detect parents
         src_parents = OntoBox.super_classes(src_class)
         tgt_parents = OntoBox.super_classes(tgt_class)
-        sup_maps = self.batch_compute_mapping(src_parents, tgt_parents)
+        sup_maps = self.batch_compute_mapping(src_parents, tgt_parents, "Parents")
 
         # detect children
         src_children = list(src_class.subclasses())
         tgt_children = list(tgt_class.subclasses())
-        sub_maps = self.batch_compute_mapping(src_children, tgt_children)
+        sub_maps = self.batch_compute_mapping(src_children, tgt_children, "Children")
 
         return sup_maps, sub_maps
 
     def batch_compute_mapping(
-        self, src_classes: List[ThingClass], tgt_classes: List[ThingClass]) -> List:
-        mappings = []
+        self, src_classes: List[ThingClass], tgt_classes: List[ThingClass], flag: str
+    ) -> Dict:
+        mappings = dict()
+        discarded_mappings = dict()
+        seen_mappings = dict()
         for src, tgt in list(product(src_classes, tgt_classes)):
-            extended_mapping = self.compute_mapping(src, tgt)  # (src_iri, tgt_iri, value)
-            if extended_mapping[2] > self.threshold:
-                mappings.append(extended_mapping)
+            mapping_str, value = self.compute_mapping(src, tgt)  # ("src_iri\ttgt_iri", value)
+            if value >= self.threshold:
+                # ensure the mapping is not previously predicted
+                mappings[mapping_str] = value
+            elif value < self.threshold and value >= 0.0:
+                discarded_mappings[mapping_str] = value
+            if value == -1.0:
+                seen_mappings[mapping_str] = value
+        print(
+            f"\t[{flag}] found mappings: valid={len(mappings)}, seen={len(seen_mappings)}, discarded={len(discarded_mappings)}"
+        )
+        # print(discarded_mappings)
         return mappings
 
     def compute_mapping(
         self, src_class: ThingClass, tgt_class: ThingClass
     ) -> Tuple[str, str, float]:
-        """compute the mapping score between src-tgt classes"""
+        """compute the mapping score between src-tgt classes
+        IMPORTANT: return a invalid mapping when existed in previously predicted set
+        """
         raise NotImplementedError
 
     def iri2class(self, iri: str, flag: str = "SRC") -> ThingClass:
@@ -80,3 +108,22 @@ class OntoExtend:
         ob = self.src_ob if flag == "SRC" else self.tgt_ob
         full_iri = ob.onto_text.expand_entity_iri(iri)
         return ob.onto.search(iri=full_iri)[0]
+
+    @classmethod
+    def read_mappings_to_dict(
+        cls, mapping_file: Union[str, DataFrame], threshold: float = 0.0
+    ) -> Dict:
+        """read unique mappings from tsv file or pandas.DataFrame, notice that for duplicated
+        mappings, the mapping value is assumed to be consistent.
+        """
+        if type(mapping_file) is DataFrame:
+            _df = mapping_file
+        else:
+            _df = pd.read_csv(mapping_file, sep="\t", na_values=cls.na_vals, keep_default_na=False)
+        mapping_dict = dict()
+        for i in range(len(_df)):
+            if _df.iloc[i][-1] >= threshold:
+                mapping_string = "\t".join(_df.iloc[i][:-1])
+                mapping_dict[mapping_string] = _df.iloc[i][-1]
+        print(f"read {len(mapping_dict)} mappings with threshold >= {threshold}.")
+        return mapping_dict
--- eval_bertmap.py
+++ eval_bertmap.py
@@ -8,8 +8,6 @@
 # append the paths
 import os
 
-from numpy.core.einsumfunc import _parse_possible_contraction
-
 main_dir = os.getcwd().split("BERTMap")[0] + "BERTMap"
 # os.environ["TOKENIZERS_PARALLELISM"] = "false"  # disable huggingface tokenizer paralellism
 import sys
@@ -21,7 +19,6 @@ import argparse
 import json
 from shutil import copy2
 from pathlib import Path
-from copy import deepcopy
 import multiprocessing_on_dill
 import pandas as pd
 
@@ -32,22 +29,15 @@ from bertmap.map import OntoMapping
 na_vals = pd.io.parsers.STR_NA_VALUES.difference({"NULL", "null", "n/a"})
 task_dir = ""
 exp_dir = ""
+map_dir = ""
+extended_set_type = None
 
-def eval_maps(config, mode, specified_candidate_limit=None, strategy=None):
+
+def eval_maps(config, mode, candidate_limit, strategy=None):
 
     global task_dir, exp_dir
     task_dir = config["data"]["task_dir"]
     exp_dir = ""
-    map_params = deepcopy(config["map"])
-    limits = map_params["candidate_limits"]
-    del map_params["candidate_limits"]
-    
-    def val_test():
-        if specified_candidate_limit:
-            validate_then_test(config=config, candidate_limit=specified_candidate_limit)
-        else:
-            for candidate_limit in limits:
-                validate_then_test(config=config, candidate_limit=candidate_limit)
 
     if mode == "bertmap":
         fine_tune_params = config["fine-tune"]
@@ -56,54 +46,66 @@ def eval_maps(config, mode, specified_candidate_limit=None, strategy=None):
         include_ids = fine_tune_params["include_ids"]
         banner(f"evaluate fine-tuned models of {learn} settings", sym="#")
         exp_dir = (
-            task_dir + f"/fine-tune.exp/{learn}.exp" if not include_ids else task_dir + f"/fine-tune.exp/{learn}.ids.exp"
+            task_dir + f"/fine-tune.exp/{learn}.exp"
+            if not include_ids
+            else task_dir + f"/fine-tune.exp/{learn}.ids.exp"
         )
-        val_test()
+        validate_then_test(config=config, candidate_limit=candidate_limit)
     elif mode == "bertembeds":
         if not strategy:
             for strt in ["cls", "mean"]:
                 exp_dir = task_dir + f"/{strt}-embeds.exp"
-                val_test()
+                validate_then_test(config=config, candidate_limit=candidate_limit)
         else:
             exp_dir = task_dir + f"/{strategy}-embeds.exp"
-            val_test()
+            validate_then_test(config=config, candidate_limit=candidate_limit)
     elif mode == "edit":
         exp_dir = task_dir + "/nes.exp"
-        val_test()
+        validate_then_test(config=config, candidate_limit=candidate_limit)
     else:
         raise ValueError("invalid option of mode ...")
 
-             
+
 def validate_then_test(config, candidate_limit: int):
+
+    global map_dir
+    map_dir = f"{exp_dir}/map.{candidate_limit}"
+    if args.extended:
+        map_dir = f"{exp_dir}/map.{candidate_limit}/extended"
+
     best_bertmap_ind, best_strm_ind = validate_maps(config=config, candidate_limit=candidate_limit)
     if not best_bertmap_ind:
         # if already generated a validation results
-        val_file = f"{exp_dir}/map.{candidate_limit}/results.val.{candidate_limit}.csv"
+        val_file = f"{map_dir}/results.val.{candidate_limit}.csv"
         val_results = pd.read_csv(val_file, index_col=0)
         best_bertmap_ind = list(val_results[:-3].idxmax()[["F1"]])[0]
         best_strm_ind = list(val_results[-3:].idxmax()[["F1"]])[0]
-        banner(f"found best hyperparameters: {best_bertmap_ind} (BERTMap) {best_strm_ind} (String-match)")
+        banner(
+            f"found best hyperparameters: {best_bertmap_ind} (BERTMap) {best_strm_ind} (String-match)"
+        )
         # OntoMapping.print_eval(val_file, "(validation)")
     # generate 70% results for both unsupervised and semi-supervised setting for comparison
     test_maps(config, candidate_limit, best_bertmap_ind, best_strm_ind, semi_supervised=True)
     if "us" in str(config["fine-tune"]["learning"]):
-            test_maps(config, candidate_limit, best_bertmap_ind, best_strm_ind, semi_supervised=False)
-    
+        test_maps(config, candidate_limit, best_bertmap_ind, best_strm_ind, semi_supervised=False)
 
-def test_maps(config, candidate_limit: int, best_hyper: str, best_strm_hyper: str, semi_supervised: bool):
-    
+
+def test_maps(
+    config, candidate_limit: int, best_hyper: str, best_strm_hyper: str, semi_supervised: bool
+):
+
     if semi_supervised:
-        eval_file = f"{exp_dir}/map.{candidate_limit}/results.test.ss.{candidate_limit}.csv"
+        eval_file = f"{map_dir}/results.test.ss.{candidate_limit}.csv"
     else:
-        eval_file = f"{exp_dir}/map.{candidate_limit}/results.test.us.{candidate_limit}.csv"
+        eval_file = f"{map_dir}/results.test.us.{candidate_limit}.csv"
     if os.path.exists(eval_file):
         print(f"skip map testing for candidate limit {candidate_limit} as existed ...")
         return
-    
+
     # select the best mapping set-threshold combination according to validation results
     set_type, threshold = best_hyper.split(":")  # src/tgt/combined:threshold
-    mapping_file = f"{exp_dir}/map.{candidate_limit}/{set_type}.{candidate_limit}.tsv"
-    
+    mapping_file = f"{map_dir}/{set_type}.{candidate_limit}.tsv"
+
     # configure reference mappings and mappings to be ignored
     ref = f"{task_dir}/refs/maps.ref.full.tsv"
     train_maps_df = pd.read_csv(
@@ -112,8 +114,10 @@ def test_maps(config, candidate_limit: int, best_hyper: str, best_strm_hyper: st
     val_maps_df = pd.read_csv(
         f"{task_dir}/refs/maps.ref.ss.val.tsv", sep="\t", na_values=na_vals, keep_default_na=False
     )
-        
-    ref_ignored = f"{task_dir}/refs/maps.ignored.tsv" if config["corpora"]["ignored_mappings_file"] else None
+
+    ref_ignored = (
+        f"{task_dir}/refs/maps.ignored.tsv" if config["corpora"]["ignored_mappings_file"] else None
+    )
     if ref_ignored:
         ref_ignored = pd.read_csv(ref_ignored, sep="\t", na_values=na_vals, keep_default_na=False)
     else:
@@ -125,14 +129,14 @@ def test_maps(config, candidate_limit: int, best_hyper: str, best_strm_hyper: st
     else:
         # only val (10%) should be ignored for unsupervised setting
         ref_ignored = ref_ignored.append(val_maps_df).reset_index(drop=True)
-        
+
     # evaluate the corresponding test-set result
     result = OntoMapping.evaluate(mapping_file, ref, ref_ignored, float(threshold), set_type)
     # evaluate the baseline string-matching results
     set_type, threshold = best_strm_hyper.split(":")  # src/tgt/combined:threshold
     result_strm = OntoMapping.evaluate(mapping_file, ref, ref_ignored, float(threshold), set_type)
     result = result.append(result_strm)
-    
+
     result.to_csv(eval_file)
     if semi_supervised:
         banner("70% test set results (semi-supervised)")
@@ -144,14 +148,16 @@ def test_maps(config, candidate_limit: int, best_hyper: str, best_strm_hyper: st
 
 def validate_maps(config, candidate_limit: int):
 
-    eval_file = f"{exp_dir}/map.{candidate_limit}/results.val.{candidate_limit}.csv"
+    eval_file = f"{map_dir}/results.val.{candidate_limit}.csv"
     if os.path.exists(eval_file):
         print(f"skip map validation for candidate limit {candidate_limit} as existed ...")
         return None, None
 
     report = pd.DataFrame(columns=["#Mappings", "#Ignored", "Precision", "Recall", "F1"])
     ref = f"{task_dir}/refs/maps.ref.full.tsv"
-    ref_ignored = f"{task_dir}/refs/maps.ignored.tsv" if config["corpora"]["ignored_mappings_file"] else None
+    ref_ignored = (
+        f"{task_dir}/refs/maps.ignored.tsv" if config["corpora"]["ignored_mappings_file"] else None
+    )
     if ref_ignored:
         ref_ignored = pd.read_csv(ref_ignored, sep="\t", na_values=na_vals, keep_default_na=False)
     else:
@@ -168,17 +174,28 @@ def validate_maps(config, candidate_limit: int):
 
     pool = multiprocessing_on_dill.Pool(10)
     eval_results = []
-    thresholds = evenly_divide(0, 0.8, 8) + evenly_divide(0.9, 0.97, 7) + evenly_divide(0.98, 1.0, 20)
+    thresholds = (
+        evenly_divide(0, 0.8, 8) + evenly_divide(0.9, 0.97, 7) + evenly_divide(0.98, 1.0, 20)
+    )
+
+    cb_map_path = f"{exp_dir}/map.{candidate_limit}/combined.{candidate_limit}.tsv"
+    src_map_path = f"{exp_dir}/map.{candidate_limit}/src.{candidate_limit}.tsv"
+    tgt_map_path = f"{exp_dir}/map.{candidate_limit}/tgt.{candidate_limit}.tsv"
+
+    if args.extended == "combined":
+        cb_map_path = f"{map_dir}/combined.{candidate_limit}.tsv"
+    elif args.extended == "src":
+        src_map_path = f"{map_dir}/src.{candidate_limit}.tsv"
+    elif args.extended == "tgt":
+        tgt_map_path = f"{map_dir}/tgt.{candidate_limit}.tsv"
+
     for threshold in thresholds:
-        # code for including the low threshold (<0.90) evaluation or not
-        # if threshold < 0.90: 
-        #     continue
         threshold = round(threshold, 6)
         eval_results.append(
             pool.apply_async(
                 OntoMapping.evaluate,
                 args=(
-                    f"{exp_dir}/map.{candidate_limit}/combined.{candidate_limit}.tsv",
+                    cb_map_path,
                     ref,
                     ref_ignored,
                     threshold,
@@ -190,7 +207,7 @@ def validate_maps(config, candidate_limit: int):
             pool.apply_async(
                 OntoMapping.evaluate,
                 args=(
-                    f"{exp_dir}/map.{candidate_limit}/src.{candidate_limit}.tsv",
+                    src_map_path,
                     ref,
                     ref_ignored,
                     threshold,
@@ -202,7 +219,7 @@ def validate_maps(config, candidate_limit: int):
             pool.apply_async(
                 OntoMapping.evaluate,
                 args=(
-                    f"{exp_dir}/map.{candidate_limit}/tgt.{candidate_limit}.tsv",
+                    tgt_map_path,
                     ref,
                     ref_ignored,
                     threshold,
@@ -225,13 +242,16 @@ def validate_maps(config, candidate_limit: int):
     best_string_match_ind = list(report[-3:].idxmax()[["F1"]])[0]
     return best_bertmap_ind, best_string_match_ind
 
+
 if __name__ == "__main__":
 
     set_seed(888)
 
     # parse configuration file and specify mode
     parser = argparse.ArgumentParser(description="run bertmap system")
-    parser.add_argument("-c", "--config", type=str, help="configuration file for bertmap system", required=True)
+    parser.add_argument(
+        "-c", "--config", type=str, help="configuration file for bertmap system", required=True
+    )
     parser.add_argument(
         "-m",
         "--mode",
@@ -240,6 +260,15 @@ if __name__ == "__main__":
         default="bertmap",
         help="preprocessing data (pre), training BERT model (train), or computing the mappings and evaluate them (map)",
     )
+    parser.add_argument(
+        "-e",
+        "--extended",
+        type=str,
+        choices={"src", "tgt", "combined", ""},
+        default="",
+        help="the best set type from first round of validation",
+    )
+    global args
     args = parser.parse_args()
 
     banner("load configurations", sym="#")
@@ -257,7 +286,6 @@ if __name__ == "__main__":
         print("config file already existed, use the existed one ...")
     else:
         copy2(args.config, config_file)
-        
-    eval_maps(config=config_json, mode=args.mode)
 
-
+    for limit in config_json["map"]["candidate_limits"]:
+        eval_maps(config=config_json, mode=args.mode, candidate_limit=limit)
